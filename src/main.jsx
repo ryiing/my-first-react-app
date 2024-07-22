f0f97c4a1f62f7bf30d586c5c82f3f5f42638533
ryiing <ryanying1102@gmail.com>
2024-07-21 20:28:04 -0400

    adding files

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import App from './List.jsx'
+// import Greeting from './Greeting.jsx'
+import App from './Button'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <Greeting /> */}
+    <App />
+  </React.StrictMode>,
+)
